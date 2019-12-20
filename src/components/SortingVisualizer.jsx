fc8fab18e8b9ef654530ee5fd92b1c0d21db586b
mingchengzack <chengmingjtt123@gmail.com>
2019-12-20 19:47:44 +0800

    initial commit (clean up unused files)

---
+++ src/components/SortingVisualizer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class SortingVisualizer extends Component {
+  state = {};
+  render() {
+    return <div></div>;
+  }
+}
+
+export default SortingVisualizer;
