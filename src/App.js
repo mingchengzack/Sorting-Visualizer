1c15d4b149c2bd16899ab4aeca3e81199e231527
mingchengzack <chengmingjtt123@gmail.com>
2019-12-20 20:50:12 +0800

    environment setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SortingVisualizer from "./components/SortingVisualizer";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
